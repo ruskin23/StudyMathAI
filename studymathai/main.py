cec326d4fb025885271dfb0634bb0fdd32e16d85
Ruskin Patel <ruskin.patel23@gmail.com>
2025-05-24 08:54:14 -0500

    now has cli arguments

--- studymathai/main.py
+++ studymathai/main.py
@@ -1,19 +1,39 @@
 import os
-
+import argparse
 from dotenv import load_dotenv
-load_dotenv()
 
 from studymathai.db import DatabaseManager
 from studymathai.utils import TextCleaner
 from studymathai.processor import BookProcessor, PageTextExtractor, BookContentExtractor, PageAwareChapterSegmentor
 from studymathai.generator import SlideGenerator
 
-def main():
-    # === 1. Setup ===
-    filename = 'bookReader.pdf'
-    fileloc = '/mnt/d/Downloads/data'
-    filepath = os.path.join(fileloc, filename)
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="StudyMathAI pipeline runner")
+    
+    parser.add_argument("--file", 
+                        type=str,
+                        required=True, 
+                        help="PDF filename")
+    
+    parser.add_argument("--data_dir", 
+                        type=str,
+                        required=True, 
+                        help="Directory containing the PDF")
+    
+    return parser.parse_args()
+
+
+def main():
+    load_dotenv()
+    args = parse_args()
+    
+    # === 1. Setup ===
+    filename = args.file
+    fileloc = args.data_dir
+    filepath = os.path.join(fileloc, filename)
+    print(f"Filepath = {filepath}")
+    
     db = DatabaseManager()
     text_cleaner = TextCleaner()
 
